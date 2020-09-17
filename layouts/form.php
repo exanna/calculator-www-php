fc119ba8693f86bfd4672b854a25339d1ac911b5
Paulina Lubasinska <lubasinska.paulina@gmail.com>
2020-09-17 15:36:24 +0200

    checkbox

--- layouts/form.php
+++ layouts/form.php
@@ -2,42 +2,34 @@
     <form action="result.php" method="post">
         <label for="wwwDesign" class="label">
             <input type="checkbox" class="check" name="wwwDesign" id="wwwDesign">
-            <span class="checkmark"></span>
             projekt graficzny strony głównej (900 PLN)
         </label>
         <label for="WPinstalation" class="label">
-            <input type="checkbox" class="check" name="WPinstalation" id="WPinstalation">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="WPinstalation" id="WPinstalation">
             instalacja WordPress + przełożenie projektu graficznego strony głównej (600 PLN)
         </label>
         <label for="rodo" class="label">
-            <input type="checkbox" class="check" id="rodo" name="rodo">
-            <span class="checkmark"></span>
+            <input type="checkbox" id="rodo" name="rodo">
             formularz kontaktowy z RODO (100 PLN)
         </label>
         <label for="google" class="label">
-            <input type="checkbox" class="check" name="google" id="google">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="google" id="google">
             integracja z Google Analytics (100 PLN)
         </label>
         <label for="googleMaps" class="label">
-            <input type="checkbox" class="check" name="googleMaps" id="googleMaps">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="googleMaps" id="googleMaps">
             integracja z API Google Maps (50 PLN)
         </label>
         <label for="seo" class="label">
-            <input type="checkbox" class="check" name="seo" id="seo">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="seo" id="seo">
             optymalizacja pod wyszukiwarki - SEO (500 PLN)
         </label>
         <label for="ssl" class="label">
-            <input type="checkbox" class="check" name="ssl" id="ssl">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="ssl" id="ssl">
             certyfikat SSL (50 PLN)
         </label>
         <label for="mailing" class="label">
-            <input type="checkbox" class="check" name="mailing" id="mailing">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="mailing" id="mailing">
             konfiguracja poczty firmowej (150 PLN)
         </label>
         <label for="page">
@@ -46,18 +38,15 @@
         </label>
         <span>realizacja podstron:</span>
         <label for="contact" class="sub label">
-            <input type="checkbox" class="check" name="contact" id="contact">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="contact" id="contact">
             kontakt (50 PLN)
         </label>
         <label for="gallery" class="sub label">
-            <input type="checkbox" class="check" name="gallery" id="gallery">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="gallery" id="gallery">
             galeria (30 PLN)
         </label>
         <label for="regulations" class="sub label">
-            <input type="checkbox" class="check" name="regulations" id="regulations">
-            <span class="checkmark"></span>
+            <input type="checkbox" name="regulations" id="regulations">
             regulamin (20 PLN)
         </label>
         <input type="submit" value="sprawdź">
